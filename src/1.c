de8cf3ca92490b68affee2a643e12fa447f0bfff
DhruvPatel9 <f20212672@goa.bits-pilani.ac.in>
2023-11-14 16:13:09 +0530

    one upd

--- src/1.c
+++ src/1.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
 
-int main()
-{
-    float number, square;
-   // printf("Please Enter any integer Value : ");
-    scanf("%f", &number);
-    square = number * number;
-    printf("square of a given number %.2f is  =  %.2f", number, square);
+// Function for binary search
+int binarySearch(int arr[], int low, int high, int target) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            return 1; // Element found
+        } else if (arr[mid] < target) {
+            low = mid + 1; // Search in the right half
+        } else {
+            high = mid - 1; // Search in the left half
+        }
+    }
+
+    return 0; // Element not found
+}
+
+int main() {
+    // Input: Number of elements
+    int n;
+ 
+    scanf("%d", &n);
+
+    // Input: Sorted array
+    int arr[n];
+ 
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input: Target element
+    int target;
+
+    scanf("%d", &target);
+
+    // Perform binary search
+    int result = binarySearch(arr, 0, n - 1, target);
+
+    // Output
+    if (result) {
+        printf("found\n");
+    } else {
+        printf("notfound\n");
+    }
+
     return 0;
 }
